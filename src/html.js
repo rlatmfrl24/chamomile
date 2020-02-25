a936a92e4600af24f6d5d0d0afce03dbb3389eba
soulkey <397love@gmail.com>
2020-02-25 16:03:25 +0900

    feed 적용 및 Search Console Meta 적용

--- src/html.js
+++ src/html.js
@@ -12,6 +12,10 @@ export default class HTML extends React.Component {
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no, minimum-scale=1, maximum-scale=2"
           />
+          <meta
+            name="google-site-verification"
+            content="VEks6e4zC_LcTsnB_1HtivHTjTazmcz5sAYaOw-PKJY"
+          />
           {this.props.headComponents}
         </head>
         <body {...this.props.bodyAttributes}>
